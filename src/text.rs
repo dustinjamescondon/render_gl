5a6decec3f471e927a99351a88406979c7ada3a4
Dustin Condon <dustin@debian.ipv6.telus.net>
2024-01-14 12:02:04 -0800

    Add texture code for text rendering

--- src/text.rs
+++ src/text.rs
@@ -1,66 +1,170 @@
 extern crate rusttype;
 use nalgebra::Vector2;
+use nalgebra_glm::{Vec2, Vec4, Mat4};
 use std::{
     collections::HashMap,
     ffi::{c_void, CString},
 };
 type Vector2i = Vector2<i32>;
-use crate::{gl_panic, glchk, REDTexture, Texture};
+use crate::{gl_panic, glchk, ArrayBuffer, Program, REDTexture, Shader, Texture, VertexArray};
 use freetype as ft;
 
-pub struct Character {
+pub struct FontContext {
+    map: HashMap<char, Character>,
+    pub pixel_height: u32,
+    text_vao: VertexArray,
+    text_vbo: ArrayBuffer,
+    text_shader: Program,
+}
+
+struct Character {
     pub texture: REDTexture,
     pub size: Vector2i,
     pub bearing: Vector2i,
     pub advance: u32,
 }
 
-pub struct FontContext {
-    pub map: HashMap<char, Character>,
-    pub pixel_height: u32,
-}
+impl FontContext {
+    pub fn new(font: &str, pixel_height: u32) -> Result<FontContext, ()> {
+        let lib = ft::Library::init().unwrap();
+        let face = lib.new_face(font, 0).unwrap();
+        face.set_pixel_sizes(0, pixel_height).unwrap();
 
-pub fn init_font(font: &str, pixel_height: u32) -> Result<FontContext, ()> {
-    let lib = ft::Library::init().unwrap();
-    let face = lib.new_face(font, 0).unwrap();
-    face.set_pixel_sizes(0, pixel_height).unwrap();
-
-    unsafe {
-        gl::PixelStorei(gl::UNPACK_ALIGNMENT, 1);
-    }
-
-    let mut map = HashMap::<char, Character>::new();
-    for i in 0_u8..128_u8 {
-
-        face.load_char(i as usize, ft::face::LoadFlag::RENDER).unwrap();
-        let glyph = face.glyph();
-
-        let texture = REDTexture::new_from_data(
-            glyph.bitmap().raw().buffer as *mut gl::types::GLvoid,
-            glyph.bitmap().width() as usize,
-            glyph.bitmap().rows() as usize,
-        );
-
-        texture.bind();
         unsafe {
-            gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_S, gl::CLAMP_TO_EDGE as i32);
-            gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_T, gl::CLAMP_TO_EDGE as i32);
-            gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MIN_FILTER, gl::LINEAR as i32);
-            gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::LINEAR as i32);
+            gl::PixelStorei(gl::UNPACK_ALIGNMENT, 1);
+        }
+
+        let mut map = HashMap::<char, Character>::new();
+        for i in 0_u8..128_u8 {
+            face.load_char(i as usize, ft::face::LoadFlag::RENDER)
+                .unwrap();
+            let glyph = face.glyph();
+
+            let texture = REDTexture::new_from_data(
+                glyph.bitmap().raw().buffer as *mut gl::types::GLvoid,
+                glyph.bitmap().width() as usize,
+                glyph.bitmap().rows() as usize,
+            );
+
+            texture.bind();
+            unsafe {
+                gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_S, gl::CLAMP_TO_EDGE as i32);
+                gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_T, gl::CLAMP_TO_EDGE as i32);
+                gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MIN_FILTER, gl::LINEAR as i32);
+                gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::LINEAR as i32);
+            }
+
+            gl_panic!();
+
+            let character = Character {
+                texture,
+                size: Vector2i::new(glyph.bitmap().width(), glyph.bitmap().rows()),
+                bearing: Vector2i::new(glyph.bitmap_left(), glyph.bitmap_top()),
+                advance: glyph.advance().x as u32,
+            };
+            map.insert(i as char, character);
+        }
+
+        let text_vao = VertexArray::new();
+        let text_vbo = ArrayBuffer::new();
+        text_vao.bind();
+        text_vbo.bind();
+        text_vbo.dynamic_draw_data(&[0_f32; 6 * 4]);
+        unsafe {
+            gl::EnableVertexAttribArray(0);
+            gl::VertexAttribPointer(
+                0,
+                4,
+                gl::FLOAT,
+                gl::FALSE,
+                (4_usize * ::std::mem::size_of::<f32>()) as gl::types::GLsizei,
+                0 as *const gl::types::GLvoid,
+            );
+            gl::BindBuffer(gl::ARRAY_BUFFER, 0);
+            text_vao.unbind();
         }
 
         gl_panic!();
 
-        let character = Character {
-            texture,
-            size: Vector2i::new(glyph.bitmap().width(), glyph.bitmap().rows()),
-            bearing: Vector2i::new(glyph.bitmap_left(), glyph.bitmap_top()),
-            advance: glyph.advance().x as u32,
-        };
-        map.insert(i as char, character);
+        Ok(FontContext {
+            map,
+            pixel_height,
+            text_shader: Program::from_src(
+                include_str!("res/text_vertex_shader.glsl"),
+                include_str!("res/text_frag_shader.glsl")
+            ).unwrap(),
+            text_vao,
+            text_vbo,
+        })
     }
 
-    Ok(FontContext { map, pixel_height })
+    pub fn render_text(&self, text: &String, pos: Vec2, scale: f32, projection: &Mat4, clr: &[f32; 3]) {
+        self.text_shader.set_used();
+        self.text_shader
+            .set_3float("textColor".to_string(), clr.clone());
+
+        self.text_shader
+            .set_uniform_glm_m4("projection".to_string(), &projection);    
+
+        unsafe {
+            gl::ActiveTexture(gl::TEXTURE0);
+            self.text_vao.bind();
+
+            let mut _pos = pos.clone();
+
+            // iterate through characters
+            for c in text.chars() {
+                let ctex = self.map.get(&c).unwrap();
+
+                let xpos = _pos.x + (ctex.bearing.x as f32) * scale;
+                let ypos = _pos.y - (ctex.size.y - ctex.bearing.y) as f32 * scale;
+
+                let w = (ctex.size.x as f32) * scale;
+                let h = (ctex.size.y as f32) * scale;
+
+                let vertices = [
+                    xpos,
+                    ypos + h,
+                    0_f32,
+                    0_f32,
+                    xpos,
+                    ypos,
+                    0_f32,
+                    1_f32,
+                    xpos + w,
+                    ypos,
+                    1_f32,
+                    1_f32,
+                    xpos,
+                    ypos + h,
+                    0_f32,
+                    0_f32,
+                    xpos + w,
+                    ypos,
+                    1_f32,
+                    1_f32,
+                    xpos + w,
+                    ypos + h,
+                    1_f32,
+                    0_f32,
+                ];
+
+                ctex.texture.bind();
+                self.text_vbo.update_data(&vertices);
+                self.text_vbo.unbind();
+                gl::DrawArrays(gl::TRIANGLES, 0, 6);
+
+                gl_panic!();
+
+                _pos.x += (ctex.advance >> 6) as f32 * scale;
+            }
+            self.text_vao.unbind();
+            unsafe {
+                gl::BindTexture(gl::TEXTURE_2D, 0);
+            }
+            gl_panic!();
+        }
+    }
 }
 
 #[cfg(test)]
--- src/texture/rgb_texture.rs
+++ src/texture/rgb_texture.rs
@@ -1,26 +1,27 @@
+use crate::glchk;
 use gl;
 use sdl2::pixels::Color;
 use std::os::raw::c_void;
-use crate::glchk;
 
 // Refer https://registry.khronos.org/OpenGL-Refpages/gl4/html/glTexImage2D.xhtml
 // and https://moderngl.readthedocs.io/en/latest/topics/texture_formats.html
 pub trait TextureType {
-	const INTERNAL_FORMAT: gl::types::GLuint;
+    const INTERNAL_FORMAT: gl::types::GLuint;
     const TEXTURE_TYPE: gl::types::GLuint;
-	const DATA_TYPE: gl::types::GLenum;
+    const DATA_TYPE: gl::types::GLenum;
+    const TARGET: gl::types::GLenum;
 }
 
 pub struct Texture<T>
-where 
-	T: TextureType
+where
+    T: TextureType,
 {
-	pub id: gl::types::GLuint,
-	pub width: gl::types::GLint,
-	pub height: gl::types::GLint,
-	pub target: gl::types::GLuint,
-	pub attach_point: gl::types::GLenum,
-	_hack: std::marker::PhantomData<T>,
+    pub id: gl::types::GLuint,
+    pub width: gl::types::GLint,
+    pub height: gl::types::GLint,
+    pub target: gl::types::GLuint,
+    pub attach_point: gl::types::GLenum,
+    _hack: std::marker::PhantomData<T>,
 }
 
 pub type RGBTexture = Texture<TextureTypeRGB>;
@@ -29,213 +30,223 @@ pub type REDTexture = Texture<TextureTypeRed>;
 
 pub struct TextureTypeRGB;
 impl TextureType for TextureTypeRGB {
-	const INTERNAL_FORMAT: gl::types::GLuint = gl::RGB4;
-	const TEXTURE_TYPE: gl::types::GLuint = gl::RGB;
-	const DATA_TYPE: gl::types::GLenum = gl::FLOAT;
+    const INTERNAL_FORMAT: gl::types::GLuint = gl::RGB4;
+    const TEXTURE_TYPE: gl::types::GLuint = gl::RGB;
+    const DATA_TYPE: gl::types::GLenum = gl::FLOAT;
+    const TARGET: gl::types::GLenum = gl::TEXTURE_RECTANGLE;
 }
 
 pub struct TextureTypeI32;
 impl TextureType for TextureTypeI32 {
-	const INTERNAL_FORMAT: gl::types::GLuint = gl::R32I;
-	const TEXTURE_TYPE: gl::types::GLuint = gl::RED_INTEGER;
-	const DATA_TYPE: gl::types::GLenum = gl::INT;
+    const INTERNAL_FORMAT: gl::types::GLuint = gl::R32I;
+    const TEXTURE_TYPE: gl::types::GLuint = gl::RED_INTEGER;
+    const DATA_TYPE: gl::types::GLenum = gl::INT;
+    const TARGET: gl::types::GLenum = gl::TEXTURE_RECTANGLE;
 }
 
 pub struct TextureTypeRed;
 impl TextureType for TextureTypeRed {
-	const INTERNAL_FORMAT: gl::types::GLuint = gl::RED;
-	const TEXTURE_TYPE: gl::types::GLuint = gl::RED;
-	const DATA_TYPE: gl::types::GLenum = gl::UNSIGNED_BYTE;
+    const INTERNAL_FORMAT: gl::types::GLuint = gl::RED;
+    const TEXTURE_TYPE: gl::types::GLuint = gl::RED;
+    const DATA_TYPE: gl::types::GLenum = gl::UNSIGNED_BYTE;
+    const TARGET: gl::types::GLenum = gl::TEXTURE_2D;
 }
 
 pub struct TextureTypeF32;
 impl TextureType for TextureTypeF32 {
-	const INTERNAL_FORMAT: gl::types::GLuint = gl::R32F;
-	const TEXTURE_TYPE: gl::types::GLuint = gl::RED;
-	const DATA_TYPE: gl::types::GLenum = gl::FLOAT;
+    const INTERNAL_FORMAT: gl::types::GLuint = gl::R32F;
+    const TEXTURE_TYPE: gl::types::GLuint = gl::RED;
+    const DATA_TYPE: gl::types::GLenum = gl::FLOAT;
+    const TARGET: gl::types::GLenum = gl::TEXTURE_2D;
 }
 
 impl Clone for RGBTexture {
     fn clone(&self) -> Self {
         RGBTexture::new_from_data(
-			self.get_pixel_data().as_mut_ptr() as *mut gl::types::GLvoid, 
-			self.width as usize, self.height as usize
-		)
+            self.get_pixel_data().as_mut_ptr() as *mut gl::types::GLvoid,
+            self.width as usize,
+            self.height as usize,
+        )
     }
 }
 
 #[allow(dead_code)]
-impl<T> Texture<T> 
-where T: TextureType {	
-	/// This allocates a texture on the video card of the given size 
-	/// without any data attached to it. This texture can be bound to 
-	/// a framebuffer and be written to by a shader
-	pub fn new(width: i32, height: i32) -> Self {
-		let target = gl::TEXTURE_2D;
-		
-		let id = create_and_set_gl_parameters();
-		unsafe {
-			gl::TexImage2D(target,
-				0,
-				T::INTERNAL_FORMAT as i32,
-				width,
-				height,
-				0,
-				T::TEXTURE_TYPE,
-				T::DATA_TYPE,
-				std::ptr::null());
-			}
-			
-			Texture {
-			id,
-			width,
-			height,
-			target,
-			attach_point: 0,
-			_hack: std::marker::PhantomData,
-		}
-	}
-	
-	/// This allocates a texture on the video card of the given size 
-	/// containing the data.
-	/// 
-	/// TODO maybe having a void pointer be necessary for this interface is a bad idea...
-	/// that is, maybe we can make specific functions for passing in color data or u32 data
-	/// instead of void*
-	pub fn new_from_data(data: *const std::ffi::c_void, width: usize, height: usize) -> Self {
-		let target = gl::TEXTURE_2D;
-		
-		let id = create_and_set_gl_parameters();
-		unsafe {
-			gl::TexImage2D(target, 
-				0,
-				T::INTERNAL_FORMAT as i32,
-				width as i32, 
-				height as i32,
-				0,
-				T::TEXTURE_TYPE,
-				T::DATA_TYPE,
-				data as *const c_void);
-			}
-		
-		Texture {
-			id,
-			width: width as i32,
-			height: height as i32,
-			target,
-			attach_point: 0,
-			_hack: std::marker::PhantomData,
-		}
-	}
-	
-	pub fn bind(&self) {
-		unsafe {
-			gl::BindTexture(gl::TEXTURE_2D, self.id);
-		}
-	}
+impl<T> Texture<T>
+where
+    T: TextureType,
+{
+    /// This allocates a texture on the video card of the given size
+    /// without any data attached to it. This texture can be bound to
+    /// a framebuffer and be written to by a shader
+    pub fn new(width: i32, height: i32) -> Self {
+        let id = Self::create_and_set_gl_parameters();
+        unsafe {
+            gl::TexImage2D(
+                T::TARGET,
+                0,
+                T::INTERNAL_FORMAT as i32,
+                width,
+                height,
+                0,
+                T::TEXTURE_TYPE,
+                T::DATA_TYPE,
+                std::ptr::null(),
+            );
+        }
 
-	pub fn unbind(&self) {
-		unsafe {
-			gl::BindTexture(gl::TEXTURE_2D, 0);
-		}
-	}
+        Texture {
+            id,
+            width,
+            height,
+            target: T::TARGET,
+            attach_point: 0,
+            _hack: std::marker::PhantomData,
+        }
+    }
 
-	pub fn get_int_pixel_data(&self) -> Vec<i32>
-	{
-		self.bind();
-		let mut data = vec![0i32; (self.width * self.height) as usize];
-		unsafe {
-			glchk!(
-			gl::GetTexImage(gl::TEXTURE_RECTANGLE,
-				0,
-				T::TEXTURE_TYPE,
-				T::DATA_TYPE,
-				data.as_mut_ptr() as *mut gl::types::GLvoid,
-			);
-		);
-		}
-		
-		data
-	}
+    /// This allocates a texture on the video card of the given size
+    /// containing the data.
+    ///
+    /// TODO maybe having a void pointer be necessary for this interface is a bad idea...
+    /// that is, maybe we can make specific functions for passing in color data or u32 data
+    /// instead of void*
+    pub fn new_from_data(data: *const std::ffi::c_void, width: usize, height: usize) -> Self {
+        let target = T::TARGET;
 
-	pub fn get_pixel_data(&self) -> Vec<f32>
-	{
-		self.bind();
-		let mut data = vec![0.0f32; (self.width * self.height * 3) as usize];
-		unsafe {
-			glchk!(
-			gl::GetTexImage(gl::TEXTURE_RECTANGLE,
-				0,
-				T::TEXTURE_TYPE,
-				T::DATA_TYPE,
-				data.as_mut_ptr() as *mut gl::types::GLvoid,
-			););
-		}
-		
-		data
-	}
-	
-	pub fn get_pixel_rgb_data(&self) -> Vec<Color>
-	{
-		self.bind();
-		let mut data = vec![0.0f32; (self.width * self.height * 3) as usize];
-		unsafe {
-			glchk!(
-			gl::GetTexImage(gl::TEXTURE_RECTANGLE,
+        let id = Self::create_and_set_gl_parameters();
+        unsafe {
+            gl::TexImage2D(
+                target,
+                0,
+                T::INTERNAL_FORMAT as i32,
+                width as i32,
+                height as i32,
+                0,
+                T::TEXTURE_TYPE,
+                T::DATA_TYPE,
+                data as *const c_void,
+            );
+        }
+
+        Texture {
+            id,
+            width: width as i32,
+            height: height as i32,
+            target,
+            attach_point: 0,
+            _hack: std::marker::PhantomData,
+        }
+    }
+
+    pub fn bind(&self) {
+        unsafe {
+            gl::BindTexture(T::TARGET, self.id);
+        }
+    }
+
+    pub fn unbind(&self) {
+        unsafe {
+            gl::BindTexture(T::TARGET, 0);
+        }
+    }
+
+    pub fn get_int_pixel_data(&self) -> Vec<i32> {
+        self.bind();
+        let mut data = vec![0i32; (self.width * self.height) as usize];
+        unsafe {
+            glchk!(
+                gl::GetTexImage(T::TARGET,
+                    0,
+                    T::TEXTURE_TYPE,
+                    T::DATA_TYPE,
+                    data.as_mut_ptr() as *mut gl::types::GLvoid,
+                );
+            );
+        }
+
+        data
+    }
+
+    pub fn get_pixel_data(&self) -> Vec<f32> {
+        self.bind();
+        let mut data = vec![0.0f32; (self.width * self.height * 3) as usize];
+        unsafe {
+            glchk!(
+			gl::GetTexImage(T::TARGET,
 				0,
 				T::TEXTURE_TYPE,
 				T::DATA_TYPE,
 				data.as_mut_ptr() as *mut gl::types::GLvoid,
 			););
-		}
-		
-		data.chunks(3)
-			.map(|arr| normalized_to_u8_color(arr[0], arr[1], arr[2])).collect()
-	}
+        }
 
-	pub fn attach_to_fbo(&mut self, attach_point: gl::types::GLenum) {
-		unsafe {
-			gl::FramebufferTexture2D(gl::FRAMEBUFFER, attach_point, self.target, self.id, 0);
-		}
-		self.attach_point = attach_point;
-	}
-	
-	pub fn attach_to_unit(&mut self, tex_unit: gl::types::GLuint) {
-		unsafe {
-			gl::ActiveTexture(gl::TEXTURE0 + tex_unit);
-			gl::BindTexture(gl::TEXTURE_2D, self.id);
-		}
-		self.attach_point = gl::TEXTURE_2D + tex_unit;
-	}
-}
+        data
+    }
 
-fn create_and_set_gl_parameters() -> gl::types::GLuint  {
-	let mut id: gl::types::GLuint = 0;
-	let target = gl::TEXTURE_2D;
+    pub fn get_pixel_rgb_data(&self) -> Vec<Color> {
+        self.bind();
+        let mut data = vec![0.0f32; (self.width * self.height * 3) as usize];
+        unsafe {
+            glchk!(
+			gl::GetTexImage(T::TARGET,
+				0,
+				T::TEXTURE_TYPE,
+				T::DATA_TYPE,
+				data.as_mut_ptr() as *mut gl::types::GLvoid,
+			););
+        }
+
+        data.chunks(3)
+            .map(|arr| normalized_to_u8_color(arr[0], arr[1], arr[2]))
+            .collect()
+    }
+
+    pub fn attach_to_fbo(&mut self, attach_point: gl::types::GLenum) {
+        unsafe {
+            gl::FramebufferTexture2D(gl::FRAMEBUFFER, attach_point, self.target, self.id, 0);
+        }
+        self.attach_point = attach_point;
+    }
+
+    pub fn attach_to_unit(&mut self, tex_unit: gl::types::GLuint) {
+        unsafe {
+            gl::ActiveTexture(gl::TEXTURE0 + tex_unit);
+            gl::BindTexture(T::TARGET, self.id);
+        }
+		// TODO shouldn't this be TEXTURE0?
+        self.attach_point = T::TARGET + tex_unit;
+    }
+
+	/// TODO impl some mech for this to be cusomized per texture, because
+	/// for instance a character texture needs specific settings
+	fn create_and_set_gl_parameters() -> gl::types::GLuint {
+		let mut id: gl::types::GLuint = 0;
+		let target = T::TARGET;
 	
-	unsafe {
-		gl::GenTextures(1, &mut id);
-		gl::BindTexture(target, id);
-		gl::TexParameteri(target, gl::TEXTURE_MIN_FILTER, gl::NEAREST as i32);
-		gl::TexParameteri(target, gl::TEXTURE_MAG_FILTER, gl::NEAREST as i32);
-		gl::TexParameteri(target, gl::TEXTURE_WRAP_S, gl::CLAMP_TO_EDGE as i32);
-		gl::TexParameteri(target, gl::TEXTURE_WRAP_T, gl::CLAMP_TO_EDGE as i32);
-	}		
-	id
+		unsafe {
+			gl::GenTextures(1, &mut id);
+			gl::BindTexture(target, id);
+			gl::TexParameteri(target, gl::TEXTURE_MIN_FILTER, gl::NEAREST as i32);
+			gl::TexParameteri(target, gl::TEXTURE_MAG_FILTER, gl::NEAREST as i32);
+			gl::TexParameteri(target, gl::TEXTURE_WRAP_S, gl::CLAMP_TO_EDGE as i32);
+			gl::TexParameteri(target, gl::TEXTURE_WRAP_T, gl::CLAMP_TO_EDGE as i32);
+		}
+		id
+	}
 }
 
 fn normalized_to_u8_color(r: f32, g: f32, b: f32) -> Color {
-	let max_u8 = u8::MAX as f32;
-	Color::RGB(
-		(r * max_u8) as u8,
-	 	(g * max_u8) as u8,
-	  	(b * max_u8) as u8)
+    let max_u8 = u8::MAX as f32;
+    Color::RGB((r * max_u8) as u8, (g * max_u8) as u8, (b * max_u8) as u8)
 }
 
-impl<T> Drop for Texture<T> 
-where T:TextureType,
+impl<T> Drop for Texture<T>
+where
+    T: TextureType,
 {
-	fn drop(&mut self) {
-		unsafe { gl::DeleteTextures(1, &self.id); }
-	}
+    fn drop(&mut self) {
+        unsafe {
+            gl::DeleteTextures(1, &self.id);
+        }
+    }
 }
