dd4faec4af34d04702bf72481c70ab44173a527c
Dustin Condon <dustin@debian.ipv6.telus.net>
2024-12-01 18:22:50 -0800

    Lint

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,21 @@
-mod shader;
-pub mod text;
 pub mod buffer;
 pub mod framebuffer;
+mod shader;
+pub mod text;
 pub mod texture;
-pub use shader::{Shader, Program};
-pub use buffer::{ ArrayBuffer, ElementArrayBuffer, VertexArray };
+pub use buffer::{ArrayBuffer, ElementArrayBuffer, VertexArray};
 pub use framebuffer::*;
+pub use shader::{Program, Shader};
 pub use texture::*;
 
 #[macro_export]
 macro_rules! gl_panic {
     () => {
-	unsafe {
-	    if gl::GetError() != gl::NO_ERROR {
-		panic!("OpenGL error!");
-	    }
-	}
+        unsafe {
+            if gl::GetError() != gl::NO_ERROR {
+                panic!("OpenGL error!");
+            }
+        }
     };
 }
 
@@ -48,22 +48,22 @@ macro_rules! glchk {
     }
 }
 
-/* 
+/*
 pub fn setup_error_callback() {
-	unsafe {
-		gl::Enable(gl::DEBUG_CALLBACK_FUNCTION);
-		gl::DebugMessageCallback(message_callback, 0)
-	}
+    unsafe {
+        gl::Enable(gl::DEBUG_CALLBACK_FUNCTION);
+        gl::DebugMessageCallback(message_callback, 0)
+    }
 }
 
 fn message_callback(
-	source: gl::types::GLenum,
-	type_: gl::types::GLenum,
-	id: gl::types::GLuint,
-	severity: gl::types::GLenum,
-	length: isize,
-	message: *const gl::types::GLchar,
-	userParam: *const gl::types::GLvoid)
-	{
+    source: gl::types::GLenum,
+    type_: gl::types::GLenum,
+    id: gl::types::GLuint,
+    severity: gl::types::GLenum,
+    length: isize,
+    message: *const gl::types::GLchar,
+    userParam: *const gl::types::GLvoid)
+    {
 
-	}*/
+    }*/
